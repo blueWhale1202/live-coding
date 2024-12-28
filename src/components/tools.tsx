c1a32783435bbf375662cccb5b965df68d2e7f38
mrfour4 <21521609@gm.uit.edu.com>
2024-12-28 17:38:55 +0700

    fix: test

--- src/components/tools.tsx
+++ src/components/tools.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 
-import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -12,40 +11,62 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { Hint } from "@/components/hint";
 import {
     AlarmClockCheck,
     BookOpen,
     DraftingCompass,
-    ListCollapse,
     MessageCircleMore,
     SquareArrowOutUpRight,
     SquareTerminal,
 } from "lucide-react";
 
-import { cn } from "@/lib/utils";
-
-import { useSidebar } from "@/stores/use-sidebar";
-import { useState } from "react";
-import { useIsClient, useMediaQuery } from "usehooks-ts";
-
 export const Tools = () => {
-    const { collapsed } = useSidebar();
-
-    const isClient = useIsClient();
-    const isMobile = useMediaQuery("(max-width: 1024px)");
-
-    const [open, setOpen] = useState(false);
-
-    const label = "More tools";
-
-    if (!isClient) {
-        return null;
-    }
-
     return (
         <div className="mt-auto">
-            <DropdownMenu open={open} onOpenChange={() => setOpen(false)}>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild className="group p-3">
+                    <SquareArrowOutUpRight />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                    side="right"
+                    align="end"
+                    className="w-48 border-[#2d2e35] bg-background"
+                >
+                    <DropdownMenuLabel>Tools</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="p-2.5" asChild>
+                        <Link href={process.env.NEXT_PUBLIC_COURSE_URL!}>
+                            <BookOpen />
+                            Courses
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="p-2.5" asChild>
+                        <Link href={process.env.NEXT_PUBLIC_FORUM_URL!}>
+                            <MessageCircleMore />
+                            Forum
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="p-2.5" asChild>
+                        <Link href={process.env.NEXT_PUBLIC_TASK_MANAGER_URL!}>
+                            <AlarmClockCheck />
+                            Task Manager
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="p-2.5" asChild>
+                        <Link href={process.env.NEXT_PUBLIC_DRAW_APP_URL!}>
+                            <DraftingCompass />
+                            Drawing App
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="p-2.5" asChild>
+                        <Link href={process.env.NEXT_PUBLIC_EDITOR_URL!}>
+                            <SquareTerminal />
+                            Code Editor
+                        </Link>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            {/* <DropdownMenu open={open} onOpenChange={() => setOpen(false)}>
                 <DropdownMenuTrigger asChild className="group p-3">
                     <div className="w-full">
                         {collapsed && (
@@ -126,7 +147,7 @@ export const Tools = () => {
                         </Link>
                     </DropdownMenuItem>
                 </DropdownMenuContent>
-            </DropdownMenu>
+            </DropdownMenu> */}
         </div>
     );
 };
