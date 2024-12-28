a90dcca0e540f2c38f441fef5c520b7ba657977b
mrfour4 <21521609@gm.uit.edu.com>
2024-12-28 22:49:23 +0700

    fix: test

--- src/components/tools.tsx
+++ src/components/tools.tsx
@@ -9,7 +9,15 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { BookOpen } from "lucide-react";
+import {
+    AlarmClockCheck,
+    BookOpen,
+    DraftingCompass,
+    MessageCircleMore,
+    SquareTerminal,
+    VideoIcon,
+} from "lucide-react";
+
 import Link from "next/link";
 
 export const Tools = () => {
@@ -25,9 +33,39 @@ export const Tools = () => {
                     <DropdownMenuLabel>Tools</DropdownMenuLabel>
                     <DropdownMenuSeparator />
                     <DropdownMenuItem className="p-2.5" asChild>
-                        <Link href={"/courses"}>
+                        <Link href={process.env.NEXT_PUBLIC_COURSE_URL!}>
                             <BookOpen />
                             Courses
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <Link href={process.env.NEXT_PUBLIC_FORUM_URL!}>
+                            <MessageCircleMore />
+                            Forum
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <Link href={process.env.NEXT_PUBLIC_TASK_MANAGER_URL!}>
+                            <AlarmClockCheck />
+                            Task Manager
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <Link href={process.env.NEXT_PUBLIC_LIVE_CODING_URL!}>
+                            <VideoIcon />
+                            Live Coding
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <Link href={process.env.NEXT_PUBLIC_DRAW_APP_URL!}>
+                            <DraftingCompass />
+                            Drawing App
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <Link href={process.env.NEXT_PUBLIC_EDITOR_URL!}>
+                            <SquareTerminal />
+                            Code Editor
                         </Link>
                     </DropdownMenuItem>
                 </DropdownMenuContent>
