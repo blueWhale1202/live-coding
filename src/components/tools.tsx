93a494d54a10df70c452423d1c27700eb486adcb
mrfour4 <21521609@gm.uit.edu.com>
2024-12-28 18:11:54 +0700

    fix: test

--- src/components/tools.tsx
+++ src/components/tools.tsx
@@ -9,60 +9,18 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import {
-    AlarmClockCheck,
-    BookOpen,
-    DraftingCompass,
-    ListCollapse,
-    MessageCircleMore,
-    SquareTerminal,
-} from "lucide-react";
-import Link from "next/link";
-
 export const Tools = () => {
     return (
         <div className="mt-auto">
             <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <ListCollapse />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                    side="right"
-                    align="end"
-                    className="w-48 border-[#2d2e35] bg-background"
-                >
-                    <DropdownMenuLabel>Tools</DropdownMenuLabel>
+                <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem className="p-2.5" asChild>
-                        <Link href={process.env.NEXT_PUBLIC_COURSE_URL!}>
-                            <BookOpen />
-                            Courses
-                        </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="p-2.5" asChild>
-                        <Link href={process.env.NEXT_PUBLIC_FORUM_URL!}>
-                            <MessageCircleMore />
-                            Forum
-                        </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="p-2.5" asChild>
-                        <Link href={process.env.NEXT_PUBLIC_TASK_MANAGER_URL!}>
-                            <AlarmClockCheck />
-                            Task Manager
-                        </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="p-2.5" asChild>
-                        <Link href={process.env.NEXT_PUBLIC_DRAW_APP_URL!}>
-                            <DraftingCompass />
-                            Drawing App
-                        </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="p-2.5" asChild>
-                        <Link href={process.env.NEXT_PUBLIC_EDITOR_URL!}>
-                            <SquareTerminal />
-                            Code Editor
-                        </Link>
-                    </DropdownMenuItem>
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
         </div>
