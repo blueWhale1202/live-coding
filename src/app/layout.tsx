8a082cba61f0714e1ffbd6a1eb85dc55d62e9c0b
mrfour4 <21521609@gm.uit.edu.com>
2024-12-28 17:58:23 +0700

    fix: test clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,48 +44,48 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <ClerkProvider
-            afterSignOutUrl="/"
-            appearance={{
-                baseTheme: dark,
-            }}
-        >
-            <html lang="en" suppressHydrationWarning>
-                <head>
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        href="/favicon/favicon-96x96.png"
-                        sizes="96x96"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/svg+xml"
-                        href="/favicon/favicon.svg"
-                    />
-                    <link rel="shortcut icon" href="/favicon/favicon.ico" />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="/favicon/apple-touch-icon.png"
-                    />
-                    <link rel="manifest" href="/favicon/site.webmanifest" />
-                </head>
-                <body className={inter.className}>
-                    <NextSSRPlugin
-                        routerConfig={extractRouterConfig(ourFileRouter)}
-                    />
-                    <ThemeProvider
-                        attribute="class"
-                        defaultTheme="dark"
-                        enableSystem
-                        disableTransitionOnChange
+        <html lang="en" suppressHydrationWarning>
+            <head>
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href="/favicon/favicon-96x96.png"
+                    sizes="96x96"
+                />
+                <link
+                    rel="icon"
+                    type="image/svg+xml"
+                    href="/favicon/favicon.svg"
+                />
+                <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon/apple-touch-icon.png"
+                />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+            </head>
+            <body className={inter.className}>
+                <NextSSRPlugin
+                    routerConfig={extractRouterConfig(ourFileRouter)}
+                />
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <ClerkProvider
+                        afterSignOutUrl="/"
+                        appearance={{
+                            baseTheme: dark,
+                        }}
                     >
                         {children}
-                        <Toaster theme="light" richColors />
-                    </ThemeProvider>
-                </body>
-            </html>
-        </ClerkProvider>
+                    </ClerkProvider>
+                    <Toaster theme="light" richColors />
+                </ThemeProvider>
+            </body>
+        </html>
     );
 }
